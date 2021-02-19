8b5c281a8c93113017c369920049f036fa2fe01a
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-19 16:21:15 -0300

    codewars daily push

---
+++ codewars-feb-19.js
@@ -0,0 +1,21 @@
+//Find the next perfect square!
+//https://www.codewars.com/kata/56269eb78ad2e4ced1000013
+function findNextSquare(sq) {
+  return Number.isInteger(Math.sqrt(sq)) ? (Math.sqrt(sq)+1) ** 2 : -1 
+}
+
+function findNextSquareBEst(sq) {
+  return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+}
+
+//Friend or Foe?
+//https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+function friend(friends){
+  return friends.filter(item => item.length === 4)
+}
+
+//Ones and Zeros
+//https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(""),2)
+};
