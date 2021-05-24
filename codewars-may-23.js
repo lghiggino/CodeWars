074bae42e0106c310e76203dacd23bb35ee851aa
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-05-23 22:18:26 -0300

    daily push

--- codewars-may-23.js
+++ codewars-may-23.js
@@ -1,3 +1,21 @@
 function noOdds( values ){
   return values.filter(item => item % 2 === 0)
 }
+
+function travel(r, zipcode) {
+  
+    if (r.indexOf(zipcode) === -1) return `${zipcode}:/`
+    else{
+      let arrayOfStreets = r.split(",")
+      .filter(item => item.includes(zipcode))
+      .map(item => item.slice(0, item.indexOf(zipcode)))
+      .map(item => item.slice(item.indexOf(" ")).trim())
+
+      let arrayOfNumbers = r.split(",")
+        .filter(item => item.includes(zipcode))
+        .map(item => item.slice(0, item.indexOf(zipcode)))
+        .map(item => item.slice(0, item.indexOf(" ")).trim())
+
+      return `${zipcode}:${arrayOfStreets.join(",")}/${arrayOfNumbers.join(",")}`
+    }
+}
